7f4aa3297a10db39b78ae5572fc7a0f5aac98069
Starscout <Polleysjonathan@gmail.com>
2024-10-22 12:29:36 -0600

    I dont remember

--- Player/player.gd
+++ Player/player.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 @export var speed = 500
 var screen_size
-var motion = 0
+var y_motion = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
@@ -21,29 +21,29 @@ func _process(delta):
 		
 		
 		
-	if motion > -350:
-		motion -= gravity
+	if y_motion > -350:
+		y_motion -= gravity
 		
 
 	if Input.is_action_pressed("down"):
 		
-		motion -= 10
+		y_motion -= 10
 		$AnimatedSprite2D.animation = "down"
-		print(motion)
+		print(y_motion)
 		
 		#We'll make this a jump button for now but I do want to change it to space bar or something
 	if Input.is_action_pressed("up"):
-		if motion < 500:
-			motion += 250
+		if y_motion < 500:
+			y_motion += 250
 		$AnimatedSprite2D.animation = "up"
-		print(motion)
+		print(y_motion)
 	
 	if velocity.length() > 0:
 		velocity = velocity * speed
 	else:
 		$AnimatedSprite2D.animation = "default"
 	
-	velocity.y -= motion
+	velocity.y -= y_motion
 	
 	position += velocity * delta 
 	position = position.clamp(Vector2.ZERO, screen_size)
