2f52ee1d2a59a265549f6f6684f0e19d7ea52424
Starscout <Polleysjonathan@gmail.com>
2024-10-24 13:51:41 -0600

    AAAAAAAAAa

--- Player/player.gd
+++ Player/player.gd
@@ -41,6 +41,11 @@ func _physics_process(delta):
 		self.velocity.y = -jump_force  # Apply negative force to jump
 		$AnimatedSprite2D.animation = "up"
 	
+	
+	
+	
+	
+	#Wall jumping physics
 	if is_on_wall_only() and Input.is_action_pressed("left"):
 		self.velocity.y = 0
 		if Input.is_action_just_pressed("up"):
