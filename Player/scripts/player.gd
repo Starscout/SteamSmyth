8eebd148aa17bd55aeda513bfceb36ffe52073b3
Starscout <Polleysjonathan@gmail.com>
2024-11-06 15:34:11 -0700

    Added THE DASH

--- Player/scripts/player.gd
+++ Player/scripts/player.gd
@@ -16,11 +16,16 @@ var is_near_left_wall = false
 var is_near_right_wall = false
 var about_to_hit_left_wall = false
 var about_to_hit_right_wall = false
+var wall_jumped = false
+var dashed = false
+var dashing = false
 var speed_up = 3
 var speed_down = 6
 var speed_change = 0
 var right_left = 0
 var respawn_location = position
+var dash_timer = 6
+var dash_change = 0
 
 signal player_path_sent(path: NodePath)
 # Called when the node enters the scene tree for the first time
@@ -36,7 +41,8 @@ func _ready():
 # Called every physics frame (handles movement and collisions)
 func _physics_process(delta):
 	# Reset horizontal velocity each frame
-	self.velocity.x = 0
+	if dashing == false:
+		self.velocity.x = 0
 	# Handle horizontal movement and animations
 	if wall_jump_timer > 0:
 		wall_jump_timer -= delta
@@ -59,10 +65,10 @@ func _physics_process(delta):
 			speed_change -= 1
 			$AnimatedSprite2D.animation = "left"
 			
-		elif Input.is_action_pressed("right"):
+		elif Input.is_action_pressed("right") and dashing == false:
 			self.velocity.x += speed
 			$AnimatedSprite2D.animation = "right"
-		elif Input.is_action_pressed("left"):
+		elif Input.is_action_pressed("left") and dashing == false:
 			self.velocity.x -= speed
 			$AnimatedSprite2D.animation = "left"
 
@@ -87,19 +93,20 @@ func _physics_process(delta):
 	if not is_on_floor():
 		self.velocity.y += gravity
 	else:
-		self.velocity.y = 0  # Reset vertical velocity when on the ground
+		if !dashing:
+			self.velocity.y = 0  # Reset vertical velocity when on the ground
 	# Vertical movement (for "down" action, can be removed if not needed)
 	if Input.is_action_pressed("down"):
 		self.velocity.y += 10
 		$AnimatedSprite2D.animation = "down"
-	if Input.is_action_just_pressed("up"):
+	if Input.is_action_just_pressed("jump"):
 		jump_buffer_timer = jump_buffer_time
 	if jump_buffer_timer > 0:
 		jump_buffer_timer -= delta
 		
 		
 		
-	if (is_on_floor() and Input.is_action_just_pressed("up")):
+	if (is_on_floor() and Input.is_action_just_pressed("jump")):
 		self.velocity.y = -jump_force
 		jump_buffer_timer = 0
 
@@ -110,7 +117,8 @@ func _physics_process(delta):
 	#Coyote
 	if (is_on_floor()):
 		coyote_timer = coyote_time
-	if (not is_on_wall() and coyote_timer > 0 and Input.is_action_just_pressed("up")):
+		dashed = false
+	if (not is_on_wall() and coyote_timer > 0 and Input.is_action_just_pressed("jump")):
 		self.velocity.y = -jump_force
 	if coyote_timer > 0:
 		coyote_timer -= delta
@@ -127,9 +135,45 @@ func _physics_process(delta):
 		wall_jump_left = -1
 	# Use move_and_slide to handle movement and collision automatically
 	move_and_slide()
-
+	
 	#Death button and save location
 	if Input.is_action_just_pressed("reset"):
 		global_position = respawn_location
+		
+	if Input.is_action_just_pressed("dash") and dashed == false:
+		wall_jump_timer = 0
+		dashed = true
+		dashing = true
+		dash_change = dash_timer
+		if Input.is_action_pressed("left") and Input.is_action_pressed("up"):
+			self.velocity.x = -750
+			self.velocity.y = -750
+
+		elif Input.is_action_pressed("left") and Input.is_action_pressed("down"):
+			self.velocity.x = -750
+			self.velocity.y = 750
+		elif Input.is_action_pressed("right") and Input.is_action_pressed("up"):
+			self.velocity.x = 750
+			self.velocity.y = -750
+		elif Input.is_action_pressed("right") and Input.is_action_pressed("down"):
+			self.velocity.x = 750
+			self.velocity.y = 750
+		elif Input.is_action_pressed("left"):
+			self.velocity.x = -1200
+			self.velocity.y = 0
+		elif Input.is_action_pressed("right"):
+			self.velocity.x = 1200
+			self.velocity.y = 0
+		elif Input.is_action_pressed("up"):
+			self.velocity.y = -900
+		elif Input.is_action_pressed("down"):
+			self.velocity.y = 900
+			
+			
+	if dash_change > 0:
+		dash_change -= 1
+	else: 
+		dashing = false
+		
 func _on_save_point_activated(new_position):
 	respawn_location = new_position
--- Rooms/main.gd
+++ Rooms/main.gd
@@ -9,7 +9,7 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 func _on_save_point_activated(position: Vector2):
 	save_location_display.update_position(position)
