d2cedba573bfc7e723bb1149cee1a8807da372d7
Starscout <Polleysjonathan@gmail.com>
2024-11-29 15:43:13 -0700

    aaaaa

--- Player/scripts/player_data.gd
+++ Player/scripts/player_data.gd
@@ -22,37 +22,6 @@ func unlock_ability(ability_name: String):
 func has_ability(ability_name: String) -> bool:
 	return abilities.get(ability_name, false)
 
-func save_game():
-	var file = FileAccess.open(save_path, FileAccess.WRITE)
-	
-	if file:
-		var json_data = JSON.stringify(abilities)
-		file.store_string(json_data)
-		file.close()
-		print ("game saved")
-	else:
-		print("Failed to save")
-		
-		
-		
-func load_game():
-	if FileAccess.file_exists(save_path):
-		var load_file = FileAccess.open(save_path, FileAccess.READ)
-		if load_file:
-			var json_data = load_file.get_as_text()
-			var json = JSON.new()
-			var result = json.parse(json_data)
-			if result.error == OK:
-				abilities = result.result
-				print ("Game loaded successfully")
-			else:
-				print("Failed to parse JSON data")
-					
-		else:
-			print("Failed to load game")
-	else:
-		print("Save file not found")
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
